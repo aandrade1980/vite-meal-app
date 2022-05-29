113688446d1311bf7760dbe45cb2b110414fdda1
Alvaro <varito1@gmail.com>
2022-05-29 16:46:58 -0300

    :sparkles: Added Google Login Flow

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,9 +1,16 @@
-import { Container, theme } from '@nextui-org/react';
+/** Component */
+import { Avatar, Container, theme } from '@nextui-org/react';
 import { Link } from 'react-router-dom';
+
+/** Context */
+import { useAuth } from '../context/AuthContext';
+
+/** Icons */
 import { MealIcon } from './MealIcon';
 
 export function Header() {
   const { zIndices } = theme;
+  const { user } = useAuth();
 
   return (
     <Container
@@ -23,10 +30,17 @@ export function Header() {
         css={{ height: 72, backdropFilter: 'blur(10px)' }}
         display="flex"
         alignItems="center"
+        justify="space-between"
       >
         <Link to="/">
           <MealIcon height={48} width={48} />
         </Link>
+        {user && (
+          <Avatar
+            src={user.photoURL || 'https://via.placeholder.com/120'}
+            size="lg"
+          />
+        )}
       </Container>
     </Container>
   );
--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
+
+/** Components */
 import App from './App';
 
+/** NextUI */
 import { NextUIProvider } from '@nextui-org/react';
 
+/** React Query */
 import { QueryClient, QueryClientProvider } from 'react-query';
 import { ReactQueryDevtools } from 'react-query/devtools';
+
+/** Context */
+import { AuthProvider } from './context/AuthContext';
 
 const queryClient = new QueryClient();
 
@@ -16,7 +23,9 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
       <QueryClientProvider client={queryClient}>
         <ReactQueryDevtools initialIsOpen={false} />
         <NextUIProvider>
-          <App />
+          <AuthProvider>
+            <App />
+          </AuthProvider>
         </NextUIProvider>
       </QueryClientProvider>
     </BrowserRouter>
