ac8dbf3eeda2d469d96ffce5fb1968757ab4d65b
Alvaro Andrade <varito1@gmail.com>
2022-08-18 15:59:16 -0300

    :white_check_mark: Adding e2e tests

--- cypress/e2e/spec.cy.ts
+++ cypress/e2e/spec.cy.ts
@@ -1,5 +1,7 @@
-describe('empty spec', () => {
-  it('passes', () => {
-    cy.visit('https://example.cypress.io');
+const baseUrl = Cypress.env('host') || 'http://127.0.0.1:5173';
+
+describe('search for a meal', () => {
+  it('should go to the home page and make a search', () => {
+    cy.visit(baseUrl);
   });
 });
