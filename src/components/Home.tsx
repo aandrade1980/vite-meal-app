2e41f609d98dfc744bd6daaf824e0fcefafcf080
Alvaro Andrade <varito1@gmail.com>
2022-08-19 13:36:17 -0300

    :art: Updated structure.

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,5 +1,9 @@
-import React, { useState } from 'react';
+/** Hooks */
+import { lazy, useState } from 'react';
 import useLocalStorage from 'use-local-storage';
+import useMeals from '../hooks/useMeals';
+
+/** Components */
 import {
   Button,
   Card,
@@ -12,10 +16,13 @@ import {
   Text
 } from '@nextui-org/react';
 
-import { Link } from 'react-router-dom';
+/** Lottie */
 import { Player } from '@lottiefiles/react-lottie-player';
 
-import useMeals from '../hooks/useMeals';
+/** Dynamic */
+const MealCardComponent = lazy(() =>
+  import('./MealCard').then(module => ({ default: module.MealCard }))
+);
 
 export function Home() {
   const [searchParam, setSearchParam] = useLocalStorage('searchParam', '');
@@ -72,30 +79,16 @@ export function Home() {
             <Loading size="xl" />
           </Container>
         ) : (
-          meals?.map(meal => (
-            <Grid key={meal.idMeal}>
-              <Link to={`/${meal.idMeal}`}>
-                <Card isHoverable isPressable>
-                  <Card.Body css={{ p: 0 }}>
-                    <Card.Image
-                      objectFit="cover"
-                      src={meal.strMealThumb}
-                      height={320}
-                      width="100%"
-                      alt={meal.strMeal}
-                    />
-                  </Card.Body>
-                  <Card.Footer>
-                    <Text b>
-                      {meal.strMeal.length > 35
-                        ? meal.strMeal.substring(0, 32) + '...'
-                        : meal.strMeal}
-                    </Text>
-                  </Card.Footer>
-                </Card>
-              </Link>
-            </Grid>
-          ))
+          <Grid.Container gap={3}>
+            {meals?.map(meal => (
+              <MealCardComponent
+                key={meal.idMeal}
+                idMeal={meal.idMeal}
+                strMealThumb={meal.strMealThumb}
+                strMeal={meal.strMeal}
+              />
+            ))}
+          </Grid.Container>
         )}
       </Grid.Container>
     </Container>
