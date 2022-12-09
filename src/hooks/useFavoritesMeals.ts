3d9a421d7a3e5d8c0720318e67f0df982c18b39a
Alvaro Andrade <varito1@gmail.com>
2022-12-09 20:28:56 -0300

    :sparkles: Filter favorites meals by user. (#31)

--- src/hooks/useFavoritesMeals.ts
+++ src/hooks/useFavoritesMeals.ts
@@ -4,21 +4,22 @@ import { useQuery } from 'react-query';
 /** Service */
 import { supabaseClient } from '../service/supabase';
 
-const getIsFavoriteMeal = async (mealId: string) => {
+const getIsFavoriteMeal = async (mealId: string, userId: string) => {
   const { count } = await supabaseClient
     .from('favorites_meals')
     .select('meal_id', { count: 'exact', head: true })
+    .filter('user_id', 'eq', userId)
     .eq('meal_id', mealId);
 
   return !!count;
 };
 
-export const useIsFavoriteMeal = (mealId: string) => {
+export const useIsFavoriteMeal = (mealId: string, userId: string) => {
   const { data, error, isLoading, refetch } = useQuery(
-    ['isFavorite', mealId],
-    () => getIsFavoriteMeal(mealId),
+    ['isFavorite', mealId, userId],
+    () => getIsFavoriteMeal(mealId, userId),
     {
-      enabled: !!mealId
+      enabled: !!mealId && !!userId
     }
   );
 
